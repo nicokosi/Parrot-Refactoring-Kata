2071884c21686e029eecafed0c59a556c028f814
Peter Kofler <peter.kofler@code-cop.org>
2017-11-04 23:22:04 +0100

    speed

--- Ruby/lib/parrot.rb
+++ Ruby/lib/parrot.rb
@@ -58,5 +58,20 @@ class AfricanParrot < Parrot
 end
 
 class NorwegianBlueParrot < Parrot
+
+  def initialize type, number_of_coconuts, voltage, nailed
+    @voltage = voltage
+    @nailed = nailed
+  end
+
+  def speed
+    return (@nailed) ? 0 : compute_base_speed_for_voltage(@voltage)
+  end
+
+  private
+
+  def compute_base_speed_for_voltage voltage
+    [24.0, voltage * base_speed].min
+  end
   
 end
