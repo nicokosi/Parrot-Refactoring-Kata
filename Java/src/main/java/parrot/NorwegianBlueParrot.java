f8d11cf808f927437107ad0e8e547a8ab52db47b
Peter Kofler <peter.kofler@code-cop.org>
2017-11-04 17:46:44 +0100

    introduce NorwegianParrot

---
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -0,0 +1,9 @@
+package parrot;
+
+public class NorwegianBlueParrot extends Parrot {
+
+    public NorwegianBlueParrot(ParrotTypeEnum _type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(_type, numberOfCoconuts, voltage, isNailed);
+    }
+
+}
