d0745274842f2ae1b9e0c234a144bc51b932ad90
Peter Kofler <peter.kofler@code-cop.org>
2017-11-04 23:20:34 +0100

    removed from base

--- Ruby/lib/parrot.rb
+++ Ruby/lib/parrot.rb
@@ -2,15 +2,12 @@ class Parrot
 
   def initialize type, number_of_coconuts, voltage, nailed
     @type = type
-    @number_of_coconuts = number_of_coconuts
     @voltage = voltage
     @nailed = nailed
   end
 
   def speed
     case @type
-    when :african_parrot
-      return [0, base_speed - load_factor * @number_of_coconuts].max
     when :norwegian_blue_parrot
       return (@nailed) ? 0 : compute_base_speed_for_voltage(@voltage)
     end
@@ -22,10 +19,6 @@ class Parrot
 
   def compute_base_speed_for_voltage voltage
     [24.0, voltage * base_speed].min
-  end
-
-  def load_factor
-    9.0
   end
 
   protected
