6bebf7729a4239af58231ce3248def98d4198ebd
Peter Kofler <peter.kofler@code-cop.org>
2017-11-06 21:51:35 +0100

    13

--- Python-2/parrot.py
+++ Python-2/parrot.py
@@ -47,6 +47,12 @@ class AfricanParrot(Parrot):
 
 class NorwegianBlueParrot(Parrot):
 
+    def __init__(self, type_of_parrot, number_of_coconuts, voltage, nailed):
+        self._type = type_of_parrot
+        self._number_of_coconuts = number_of_coconuts
+        self._voltage = voltage
+        self._nailed = nailed
+
     def speed(self):
         if self._nailed:
             return 0
