cef9f933ff964d370a24d71670054131f1a65f7f
Peter Kofler <peter.kofler@code-cop.org>
2017-11-08 17:52:46 +0100

    11b change method sig

--- Java-2/src/main/java/parrot/AfricanParrot.java
+++ Java-2/src/main/java/parrot/AfricanParrot.java
@@ -4,7 +4,7 @@ public class AfricanParrot extends Parrot {
 
     private final int numberOfCoconuts;
 
-    public AfricanParrot(ParrotTypeEnum _type, int numberOfCoconuts, double voltage, boolean isNailed) {
+    public AfricanParrot(int numberOfCoconuts) {
         super(null, 0, 0, false);
         this.numberOfCoconuts = numberOfCoconuts;
     }
