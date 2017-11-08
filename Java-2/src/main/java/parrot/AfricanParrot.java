f66a74f90a26362d082b73ae0b33515446b97da2
Peter Kofler <peter.kofler@code-cop.org>
2017-11-08 17:48:47 +0100

    7

--- Java-2/src/main/java/parrot/AfricanParrot.java
+++ Java-2/src/main/java/parrot/AfricanParrot.java
@@ -4,4 +4,9 @@ public class AfricanParrot extends Parrot {
     public AfricanParrot(ParrotTypeEnum _type, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(_type, numberOfCoconuts, voltage, isNailed);
     }
+
+    public double getSpeed() {
+        return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
+    }
+
 }
