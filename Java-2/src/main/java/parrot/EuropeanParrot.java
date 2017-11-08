77920284fe44023743601deb7a96bfbe6b78428d
Peter Kofler <peter.kofler@code-cop.org>
2017-11-08 17:42:57 +0100

    2

--- Java-2/src/main/java/parrot/EuropeanParrot.java
+++ Java-2/src/main/java/parrot/EuropeanParrot.java
@@ -4,4 +4,9 @@ public class EuropeanParrot extends Parrot {
     public EuropeanParrot(ParrotTypeEnum _type, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(_type, numberOfCoconuts, voltage, isNailed);
     }
+
+    public double getSpeed() {
+        return getBaseSpeed();
+    }
+
 }
