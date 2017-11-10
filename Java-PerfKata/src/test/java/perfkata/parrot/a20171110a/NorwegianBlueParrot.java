4b958f83a95714c9f884950a6a1e6fa60ae70373
Peter Kofler <peter.kofler@code-cop.org>
2017-11-10 23:29:12 +0100

    26

--- Java-PerfKata/src/test/java/perfkata/parrot/a20171110a/NorwegianBlueParrot.java
+++ Java-PerfKata/src/test/java/perfkata/parrot/a20171110a/NorwegianBlueParrot.java
@@ -4,7 +4,7 @@ public class NorwegianBlueParrot extends Parrot {
     private double voltage;
     private boolean isNailed;
 
-    public NorwegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+    public NorwegianBlueParrot(double voltage, boolean isNailed) {
         super(null, 0, 0, false);
         this.voltage = voltage;
         this.isNailed = isNailed;
