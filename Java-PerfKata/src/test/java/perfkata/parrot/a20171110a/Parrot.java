11d43532198b66b321b849d8b951530995feca41
Peter Kofler <peter.kofler@code-cop.org>
2017-11-10 23:30:36 +0100

    30

--- Java-PerfKata/src/test/java/perfkata/parrot/a20171110a/Parrot.java
+++ Java-PerfKata/src/test/java/perfkata/parrot/a20171110a/Parrot.java
@@ -2,9 +2,6 @@ package perfkata.parrot.a20171110a;
 
 public class Parrot {
 
-    public Parrot() {
-    }
-
     public double getSpeed() {
         throw new RuntimeException("Should be unreachable");
     }
