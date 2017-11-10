874b9d5fd573db13b3f565e5c101521a273f819e
Peter Kofler <peter.kofler@code-cop.org>
2017-11-10 23:11:18 +0100

    4

--- Java-PerfKata/src/test/java/perfkata/parrot/a20171110a/EuropeanParrot.java
+++ Java-PerfKata/src/test/java/perfkata/parrot/a20171110a/EuropeanParrot.java
@@ -2,7 +2,7 @@ package perfkata.parrot.a20171110a;
 
 public class EuropeanParrot extends Parrot {
     public EuropeanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(type, numberOfCoconuts, voltage, isNailed);
+        super(null, 0, 0, false);
     }
 
     @Override
