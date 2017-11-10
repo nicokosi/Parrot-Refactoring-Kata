e82402b7bea9e46c5317a786513c2409ad129386
Peter Kofler <peter.kofler@code-cop.org>
2017-11-10 23:23:07 +0100

    16

--- Java-PerfKata/src/test/java/perfkata/parrot/a20171110a/AfricanParrot.java
+++ Java-PerfKata/src/test/java/perfkata/parrot/a20171110a/AfricanParrot.java
@@ -4,7 +4,7 @@ public class AfricanParrot extends Parrot {
 
     private int numberOfCoconuts;
 
-    public AfricanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+    public AfricanParrot(int numberOfCoconuts) {
         super(null, 0, 0, false);
         this.numberOfCoconuts = numberOfCoconuts;
     }
