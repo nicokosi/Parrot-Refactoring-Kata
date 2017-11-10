dd6ffb7841651602e7daaee5958929e59cf4037a
Peter Kofler <peter.kofler@code-cop.org>
2017-11-10 23:45:53 +0100

    7

---
+++ Java-PerfKata/src/test/java/perfkata/parrot/a20171110b/EuropeanParrot.java
@@ -0,0 +1,13 @@
+package perfkata.parrot.a20171110b;
+
+public class EuropeanParrot extends Parrot {
+    public EuropeanParrot() {
+        super(null, 0, 0, false);
+    }
+
+    @Override
+    public double getSpeed() {
+            return getBaseSpeed();
+    }
+
+}
--- Java-PerfKata/src/test/java/perfkata/parrot/a20171110b/ParrotTypeEnum.java
+++ Java-PerfKata/src/test/java/perfkata/parrot/a20171110b/ParrotTypeEnum.java
@@ -2,7 +2,6 @@ package perfkata.parrot.a20171110b;
 
 public enum ParrotTypeEnum {
 
-    EUROPEAN,
     AFRICAN,
     NORWEGIAN_BLUE
 }
