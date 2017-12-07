76315d5e3887f8e864d6cbbcb451b777a2eda244
Peter Kofler <peter.kofler@code-cop.org>
2017-12-07 23:45:42 +0100

    Norwegian and Parrot and Cleanup.

--- Python-PerfKata/perfkata/parrot/a20171207/test_parrot.py
+++ Python-PerfKata/perfkata/parrot/a20171207/test_parrot.py
@@ -1,6 +1,6 @@
 import nose
 
-from parrot import Parrot, ParrotType, EuropeanParrot, AfricanParrot
+from parrot import EuropeanParrot, AfricanParrot, NorwegianBlueParrot
 
 
 def test_speedOfEuropeanParrot():
@@ -24,17 +24,17 @@ def test_speedOfAfricanParrot_With_No_Coconuts():
 
 
 def test_speedNorwegianBlueParrot_nailed():
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 1.5, True)
+    parrot = NorwegianBlueParrot(1.5, True)
     assert parrot.speed() == 0.0
 
 
 def test_speedNorwegianBlueParrot_not_nailed():
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 1.5, False)
+    parrot = NorwegianBlueParrot(1.5, False)
     assert parrot.speed() == 18.0
 
 
 def test_speedNorwegianBlueParrot_not_nailed_high_voltage():
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 4, False)
+    parrot = NorwegianBlueParrot(4, False)
     assert parrot.speed() == 24.0
 
 
