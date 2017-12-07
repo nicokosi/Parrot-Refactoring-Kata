accb697da3476a18086a96c32d24ebf6d18b69af
Peter Kofler <peter.kofler@code-cop.org>
2017-12-07 23:12:38 +0100

    African.

---
+++ Java-PerfKata/src/test/java/perfkata/parrot/a20171207b/AfricanParrot.java
@@ -0,0 +1,25 @@
+package perfkata.parrot.a20171207b;
+
+public class AfricanParrot extends Parrot {
+
+    private final int numberOfCoconuts;
+
+    public AfricanParrot(int numberOfCoconuts) {
+        super(null, 0, 0, false);
+        this.numberOfCoconuts = numberOfCoconuts;
+    }
+
+    @Override
+    public double getSpeed() {
+        return Math.max(0, getBaseSpeed() - slowdownDueCoconuts());
+    }
+
+    private double slowdownDueCoconuts() {
+        return getLoadFactor() * numberOfCoconuts;
+    }
+
+    private double getLoadFactor() {
+        return 9.0;
+    }
+
+}
--- Java-PerfKata/src/test/java/perfkata/parrot/a20171207b/ParrotTypeEnum.java
+++ Java-PerfKata/src/test/java/perfkata/parrot/a20171207b/ParrotTypeEnum.java
@@ -2,6 +2,5 @@ package perfkata.parrot.a20171207b;
 
 public enum ParrotTypeEnum {
 
-    AFRICAN,
     NORWEGIAN_BLUE
 }
