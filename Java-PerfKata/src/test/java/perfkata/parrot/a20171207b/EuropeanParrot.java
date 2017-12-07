c2fd6698e13f26272919983e8a708a9fd90bc6f9
Peter Kofler <peter.kofler@code-cop.org>
2017-12-07 23:17:40 +0100

    Cleanup.

--- Java-PerfKata/src/test/java/perfkata/parrot/a20171207b/EuropeanParrot.java
+++ Java-PerfKata/src/test/java/perfkata/parrot/a20171207b/EuropeanParrot.java
@@ -2,10 +2,6 @@ package perfkata.parrot.a20171207b;
 
 public class EuropeanParrot extends Parrot {
 
-    public EuropeanParrot() {
-        super();
-    }
-
     @Override
     public double getSpeed() {
         return getBaseSpeed();
