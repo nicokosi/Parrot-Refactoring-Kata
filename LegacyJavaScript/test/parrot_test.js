d637ea6007a3926a115c6976f9da8290f0d072fd
Peter Kofler <peter.kofler@code-cop.org>
2017-12-21 22:37:01 +0100

    Rename "name space" in test.

--- LegacyJavaScript/test/parrot_test.js
+++ LegacyJavaScript/test/parrot_test.js
@@ -1,49 +1,49 @@
 /* global require, describe, it */
 
 var expect = require('chai').expect;
-var Parrot = require('../src/parrot');
+var parrots = require('../src/parrot');
 
-describe("Parrot", function () {
+describe("Parrot", function() {
     'use strict';
 
-    it("gets speed of European Parrot", function () {
-        var parrot = new Parrot.EuropeanParrot();
+    it("gets speed of European Parrot", function() {
+        var parrot = new parrots.EuropeanParrot();
 
         expect(parrot.getSpeed()).eql(12);
     });
 
-    it("gets speed of African Parrot with one coconut", function () {
-        var parrot = new Parrot.AfricanParrot(1);
+    it("gets speed of African Parrot with one coconut", function() {
+        var parrot = new parrots.AfricanParrot(1);
 
         expect(parrot.getSpeed()).eql(3);
     });
 
-    it("gets speed of African Parrot with two coconuts", function () {
-        var parrot = new Parrot.AfricanParrot(2);
+    it("gets speed of African Parrot with two coconuts", function() {
+        var parrot = new parrots.AfricanParrot(2);
 
         expect(parrot.getSpeed()).eql(0);
     });
 
-    it("gets speed of African Parrot with no coconuts", function () {
-        var parrot = new Parrot.AfricanParrot(0);
+    it("gets speed of African Parrot with no coconuts", function() {
+        var parrot = new parrots.AfricanParrot(0);
 
         expect(parrot.getSpeed()).eql(12);
     });
 
-    it("gets speed of Norwegian Blue Parrot nailed", function () {
-        var parrot = new Parrot.NorwegianBlueParrot(1.5, true);
+    it("gets speed of Norwegian Blue Parrot nailed", function() {
+        var parrot = new parrots.NorwegianBlueParrot(1.5, true);
 
         expect(parrot.getSpeed()).eql(0);
     });
 
-    it("gets speed of Norwegian Blue Parrot not nailed", function () {
-        var parrot = new Parrot.NorwegianBlueParrot(1.5, false);
+    it("gets speed of Norwegian Blue Parrot not nailed", function() {
+        var parrot = new parrots.NorwegianBlueParrot(1.5, false);
 
         expect(parrot.getSpeed()).eql(18);
     });
 
-    it("gets speed of Norwegian Blue Parrot not nailed high voltage", function () {
-        var parrot = new Parrot.NorwegianBlueParrot(4, false);
+    it("gets speed of Norwegian Blue Parrot not nailed high voltage", function() {
+        var parrot = new parrots.NorwegianBlueParrot(4, false);
 
         expect(parrot.getSpeed()).eql(24);
     });
