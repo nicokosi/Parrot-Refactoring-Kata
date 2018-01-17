073155e05077876279035ace110611a943dc81ba
Peter Kofler <peter.kofler@code-cop.org>
2018-01-18 00:47:46 +0100

    Remove old method.

--- Clojure/src/parrot_refactoring/core.clj
+++ Clojure/src/parrot_refactoring/core.clj
@@ -7,15 +7,6 @@
 (defn- compute-base-speed-for-voltage [voltage]
   (min 24.0 (* voltage base-speed)))
 
-(defn speedx [parrot]
-  (case (:type parrot)
-    :european-parrot base-speed
-    :african-parrot (max 0.0 (- base-speed (* load-factor (:num-coconuts parrot))))
-    :norwegian-blue-parrot (if (:nailed parrot)
-                             0.0
-                             (compute-base-speed-for-voltage (:voltage parrot)))
-    (throw (Exception. "Should be unreachable!"))))
-
 (defrecord EuropeanParrot [])
 (defrecord AfricanParrot [num-coconuts])
 (defrecord NorwegianBlueParrot [voltage nailed])
