a86d02a4d82863afbf2b6d5e3b845fe1498ceb7a
Peter Kofler <peter.kofler@code-cop.org>
2018-12-09 17:39:25 +0100

    Revert last change.

--- Go/parrot.go
+++ Go/parrot.go
@@ -16,52 +16,52 @@ func (parrot baseParrot) baseSpeed() float64 {
 	return 12.0
 }
 
-type EuropeanParrot struct {
+type europeanParrot struct {
 	base baseParrot
 }
 
-func CreateEuropeanParrot() EuropeanParrot {
-	return EuropeanParrot{baseParrot{}}
+func CreateEuropeanParrot() europeanParrot {
+	return europeanParrot{baseParrot{}}
 }
 
-func (parrot EuropeanParrot) Speed() (float64, error) {
+func (parrot europeanParrot) Speed() (float64, error) {
 	return parrot.base.baseSpeed(), nil
 }
 
-type AfricanParrot struct {
+type africanParrot struct {
 	base             baseParrot
 	numberOfCoconuts int
 }
 
-func CreateAfricanParrot(numberOfCoconuts int) AfricanParrot {
-	return AfricanParrot{baseParrot{}, numberOfCoconuts}
+func CreateAfricanParrot(numberOfCoconuts int) africanParrot {
+	return africanParrot{baseParrot{}, numberOfCoconuts}
 }
 
-func (parrot AfricanParrot) Speed() (float64, error) {
+func (parrot africanParrot) Speed() (float64, error) {
 	return math.Max(0, parrot.base.baseSpeed()-parrot.loadFactor()*float64(parrot.numberOfCoconuts)), nil
 }
 
-func (parrot AfricanParrot) loadFactor() float64 {
+func (parrot africanParrot) loadFactor() float64 {
 	return 9.0
 }
 
-type NorwegianBlueParrot struct {
+type norwegianBlueParrot struct {
 	base    baseParrot
 	voltage float64
 	nailed  bool
 }
 
-func CreateNorwegianBlueParrot(voltage float64, nailed bool) NorwegianBlueParrot {
-	return NorwegianBlueParrot{baseParrot{}, voltage, nailed}
+func CreateNorwegianBlueParrot(voltage float64, nailed bool) norwegianBlueParrot {
+	return norwegianBlueParrot{baseParrot{}, voltage, nailed}
 }
 
-func (parrot NorwegianBlueParrot) Speed() (float64, error) {
+func (parrot norwegianBlueParrot) Speed() (float64, error) {
 	if parrot.nailed {
 		return 0, nil
 	}
 	return parrot.computeBaseSpeedForVoltage(parrot.voltage), nil
 }
 
-func (parrot NorwegianBlueParrot) computeBaseSpeedForVoltage(voltage float64) float64 {
+func (parrot norwegianBlueParrot) computeBaseSpeedForVoltage(voltage float64) float64 {
 	return math.Min(24.0, voltage*parrot.base.baseSpeed())
 }
