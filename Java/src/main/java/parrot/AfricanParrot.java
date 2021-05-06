842d8ca955024a396e4c71116bdb4b9996472eaa
Gregor Riegler <gregor.riegler@ebcont.com>
2021-05-06 19:16:30 +0200

    refactor: copy speed logic to AfricanParrot

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -1,7 +1,28 @@
 package parrot;
 
 public class AfricanParrot extends Parrot {
+
+    private int numberOfCoconuts;
+
     public AfricanParrot(int numberOfCoconuts) {
         super(ParrotTypeEnum.AFRICAN, numberOfCoconuts, 0, false);
+        this.numberOfCoconuts = numberOfCoconuts;
+    }
+
+    @Override
+    public double getSpeed() {
+        return africanSpeed();
+    }
+
+    private double africanSpeed() {
+        return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
+    }
+
+    private double getLoadFactor() {
+        return 9.0;
+    }
+
+    private double getBaseSpeed() {
+        return 12.0;
     }
 }
