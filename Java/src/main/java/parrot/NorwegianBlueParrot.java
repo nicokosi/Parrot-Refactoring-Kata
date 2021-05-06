00ac2d680343bbd4e7c3db8edbd2ca2c33e1b3bd
Gregor Riegler <gregor.riegler@ebcont.com>
2021-05-06 19:24:43 +0200

    refactor: cleanup NorwegianBlueParrot

--- Java/src/main/java/parrot/NorwegianBlueParrot.java
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -6,16 +6,15 @@ public class NorwegianBlueParrot extends Parrot {
     private boolean isNailed;
 
     public NorwegianBlueParrot(double voltage, boolean isNailed) {
-        super();
         this.voltage = voltage;
         this.isNailed = isNailed;
     }
 
     public double getSpeed() {
-        return (isNailed) ? 0 : getBaseSpeed(voltage);
+        return (isNailed) ? 0 : getVoltageSpeed();
     }
 
-    private double getBaseSpeed(double voltage) {
+    private double getVoltageSpeed() {
         return Math.min(24.0, voltage * getBaseSpeed());
     }
 }
