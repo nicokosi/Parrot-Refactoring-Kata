8abd387d0229e58cddabafa2a6d706d2a2375ef4
rea <reasu@pm.me>
2021-05-06 19:25:50 +0200

    use Norwegian in yet another Test

--- Java/src/test/java/parrot/ParrotTest.java
+++ Java/src/test/java/parrot/ParrotTest.java
@@ -44,7 +44,7 @@ public class ParrotTest {
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed_high_voltage() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false);
+        Parrot parrot = new NorwegianBlue(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false);
         assertEquals(24.0, parrot.getSpeed(), 0.0);
     }
 }
