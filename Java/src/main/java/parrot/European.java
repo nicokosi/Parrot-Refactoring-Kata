a6f887b408b135969aa09207f99dd334bb5506ed
rea <reasu@pm.me>
2021-05-06 19:19:34 +0200

    European extends Parrot

--- Java/src/main/java/parrot/European.java
+++ Java/src/main/java/parrot/European.java
@@ -1,4 +1,7 @@
 package parrot;
 
-public class European {
+public class European extends Parrot {
+    public European(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(type, numberOfCoconuts, voltage, isNailed);
+    }
 }
