d83c031fb98c152c3e05d5563fa9dc019db4cf6c
Gregor Riegler <gregor.riegler@ebcont.com>
2021-05-06 19:23:27 +0200

    refactor: cleanup EuropeanParrot

--- Java/src/main/java/parrot/EuropeanParrot.java
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -1,16 +1,10 @@
 package parrot;
 
 public class EuropeanParrot extends Parrot {
-    public EuropeanParrot() {
-        super();
-    }
 
     @Override
     public double getSpeed() {
-        return europeanSpeed();
-    }
-
-    private double europeanSpeed() {
         return getBaseSpeed();
     }
+
 }
