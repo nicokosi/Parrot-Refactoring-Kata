b0d78dc383c108ada1464485069dfdc21903964b
Gregor Riegler <gregor.riegler@ebcont.com>
2021-05-06 19:18:41 +0200

    refactor: inline africanParrotSpeed

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -11,10 +11,6 @@ public class AfricanParrot extends Parrot {
 
     @Override
     public double getSpeed() {
-        return africanSpeed();
-    }
-
-    private double africanSpeed() {
         return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
     }
 
