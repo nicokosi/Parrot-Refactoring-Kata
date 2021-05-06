fb58c55f4ca17a1c813c1096c9301b42e59811f3
Gregor Riegler <gregor.riegler@ebcont.com>
2021-05-06 19:18:17 +0200

    refactor: copy speed logic to NorwegianBlueParrot

--- Java/src/main/java/parrot/NorwegianBlueParrot.java
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -1,8 +1,26 @@
 package parrot;
 
 public class NorwegianBlueParrot extends Parrot {
+
+    private double voltage;
+    private boolean isNailed;
+
     public NorwegianBlueParrot(double voltage, boolean isNailed) {
         super(ParrotTypeEnum.NORWEGIAN_BLUE, 0, voltage, isNailed);
+        this.voltage = voltage;
+        this.isNailed = isNailed;
+    }
+
+    public double getSpeed() {
+        return (isNailed) ? 0 : getBaseSpeed(voltage);
+    }
+
+    private double getBaseSpeed(double voltage) {
+        return Math.min(24.0, voltage * getBaseSpeed());
+    }
+
+    private double getBaseSpeed() {
+        return 12.0;
     }
 
 }
