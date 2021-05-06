1f1d3a88233c9e87a6f4ceacd94e058a49929997
rea <reasu@pm.me>
2021-05-06 19:20:28 +0200

    add field isNailed

--- Java/src/main/java/parrot/NorwegianBlue.java
+++ Java/src/main/java/parrot/NorwegianBlue.java
@@ -1,7 +1,10 @@
 package parrot;
 
 public class NorwegianBlue extends Parrot{
+    private final boolean isNailed;
+
     public NorwegianBlue(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(type, numberOfCoconuts, voltage, isNailed);
+        this.isNailed = isNailed;
     }
 }
