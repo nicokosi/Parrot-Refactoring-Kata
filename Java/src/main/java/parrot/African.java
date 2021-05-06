b63c5485b90ebe71a1f7e05913bce86f5ecb58cf
rea <reasu@pm.me>
2021-05-06 19:03:58 +0200

    created class African

---
+++ Java/src/main/java/parrot/African.java
@@ -0,0 +1,7 @@
+package parrot;
+
+public class African extends Parrot {
+    public African(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(type, numberOfCoconuts, voltage, isNailed);
+    }
+}
