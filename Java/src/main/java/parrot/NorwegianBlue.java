5f937438e73f084e52f5dc9ccbc11e1d48583f33
rea <reasu@pm.me>
2021-05-06 19:04:20 +0200

    created class NorwegianBlue

---
+++ Java/src/main/java/parrot/NorwegianBlue.java
@@ -0,0 +1,7 @@
+package parrot;
+
+public class NorwegianBlue extends Parrot{
+    public NorwegianBlue(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(type, numberOfCoconuts, voltage, isNailed);
+    }
+}
