ffc7c1852d3e55ab46c4d114b29857d31694c468
rea <reasu@pm.me>
2021-05-06 19:21:35 +0200

    add field voltage

--- Java/src/main/java/parrot/NorwegianBlue.java
+++ Java/src/main/java/parrot/NorwegianBlue.java
@@ -2,9 +2,11 @@ package parrot;
 
 public class NorwegianBlue extends Parrot{
     private final boolean isNailed;
+    private final double voltage;
 
     public NorwegianBlue(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(type, numberOfCoconuts, voltage, isNailed);
         this.isNailed = isNailed;
+        this.voltage = voltage;
     }
 }
