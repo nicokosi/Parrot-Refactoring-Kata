98b20be6109d166eefb1171e6283907cc749edf5
Gregor Riegler <gregor.riegler@ebcont.com>
2021-05-06 19:20:28 +0200

    refactor: remove unused private methods

--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -16,22 +16,6 @@ public abstract class Parrot {
 
     public abstract double getSpeed();
 
-    private double norwegianBlueSpeed() {
-        return (isNailed) ? 0 : getBaseSpeed(voltage);
-    }
-
-    private double africanSpeed() {
-        return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-    }
-
-    private double getBaseSpeed(double voltage) {
-        return Math.min(24.0, voltage * getBaseSpeed());
-    }
-
-    private double getLoadFactor() {
-        return 9.0;
-    }
-
     private double getBaseSpeed() {
         return 12.0;
     }
