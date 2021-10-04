10bcad81ab9273ab5b17c1c18311d99a933e0b2e
JulioJu <11527844+JulioJu@users.noreply.github.com>
2021-10-04 19:06:05 +0200

    refactor: create getSpeed parrot-european

--- TypeScript/src/parrot-european.ts
+++ TypeScript/src/parrot-european.ts
@@ -1,11 +1,11 @@
 import { Parrot, ParrotTypes } from "./parrot";
 
 export class EuropeanParrot extends Parrot {
-  constructor(
-    numberOfCoconuts: number,
-    voltage: number,
-    isNailed: boolean
-  ) {
-      super(ParrotTypes.EUROPEAN, numberOfCoconuts, voltage, isNailed);
+  constructor(numberOfCoconuts: number, voltage: number, isNailed: boolean) {
+    super(ParrotTypes.EUROPEAN, numberOfCoconuts, voltage, isNailed);
+  }
+
+  public getSpeed(): number {
+    return this.getBaseSpeed();
   }
 }
