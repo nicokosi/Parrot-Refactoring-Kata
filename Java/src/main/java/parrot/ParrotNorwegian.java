f6d4359ffa45e93e74440cecdaa3a33648bbeaf4
johan.martinsson <johan.martinsson@dexyos.fr>
2021-10-04 19:42:31 +0200

    assign parameters to fields refactoring

--- Java/src/main/java/parrot/ParrotNorwegian.java
+++ Java/src/main/java/parrot/ParrotNorwegian.java
@@ -1,8 +1,13 @@
 package parrot;
 
 public class ParrotNorwegian extends Parrot {
+    private final double voltage;
+    private final boolean isNailed;
+
     public ParrotNorwegian(ParrotTypeEnum norwegianBlue, double voltage, boolean isNailed) {
         super(norwegianBlue, voltage, isNailed);
+        this.voltage = voltage;
+        this.isNailed = isNailed;
     }
 
     @Override
