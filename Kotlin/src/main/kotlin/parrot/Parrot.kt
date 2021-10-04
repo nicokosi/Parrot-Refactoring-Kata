8609f56f914307d4e91d3696e78fdd30634cab61
Bastien David <bastien.david@sundayapp.com>
2021-10-04 19:23:22 +0200

    refactor step

--- Kotlin/src/main/kotlin/parrot/Parrot.kt
+++ Kotlin/src/main/kotlin/parrot/Parrot.kt
@@ -13,13 +13,17 @@ open class Parrot(
     val speed: Double
         get() = when (type) {
             ParrotTypeEnum.EUROPEAN -> EuropeanParrot(baseSpeed).speed
-            ParrotTypeEnum.AFRICAN -> africanSpeed(baseSpeed, loadFactor)
+            ParrotTypeEnum.AFRICAN -> africanSpeed(baseSpeed, loadFactor, numberOfCoconuts)
             ParrotTypeEnum.NORWEGIAN_BLUE -> norwegianBlueSpeed()
         }
 
     private fun norwegianBlueSpeed() = if (isNailed) 0.0 else getBaseSpeed(voltage)
 
-    private fun africanSpeed(baseSpeed: Double, loadFactor: Double) = max(0.0, baseSpeed - loadFactor * numberOfCoconuts)
+    private fun africanSpeed(
+        baseSpeed: Double,
+        loadFactor: Double,
+        numberOfCoconuts: Int
+    ) = max(0.0, baseSpeed - loadFactor * numberOfCoconuts)
 
     private val loadFactor: Double
         get() = 9.0
