c3e10c5e33b930fa72c6cc324d0d975d76bd8c08
Bastien David <bastien.david@sundayapp.com>
2021-10-04 19:04:52 +0200

    refactor step

--- Kotlin/src/main/kotlin/parrot/Parrot.kt
+++ Kotlin/src/main/kotlin/parrot/Parrot.kt
@@ -29,9 +29,9 @@ open class Parrot(
 
     private fun getBaseSpeed(voltage: Double): Double = min(24.0, voltage * baseSpeed)
 
-    private fun europeanSpeed(baseSpeed: Double) = baseSpeed
+    private fun europeanSpeed(baseSpeed: Double) = EuropeanParrot().europeanSpeed(baseSpeed)
 }
 
 class EuropeanParrot {
-    private fun europeanSpeed(baseSpeed: Double) = baseSpeed
+    fun europeanSpeed(baseSpeed: Double) = baseSpeed
 }
