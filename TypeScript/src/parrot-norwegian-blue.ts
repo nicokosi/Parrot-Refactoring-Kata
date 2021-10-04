6a2665d7230404dd24b2b2700edcaabc95398b96
JulioJu <11527844+JulioJu@users.noreply.github.com>
2021-10-04 19:42:02 +0200

    refactor: baseSpeedWithVoltage function is write protected

--- TypeScript/src/parrot-norwegian-blue.ts
+++ TypeScript/src/parrot-norwegian-blue.ts
@@ -4,10 +4,10 @@ export class NorwegianBlueParrot extends Parrot {
   constructor(protected voltage: number, protected isNailed: boolean) {
     super();
   }
-  private getBaseSpeedWithVoltage(): number {
+  private get baseSpeedWithVoltage() {
     return Math.min(24, this.voltage * this.baseSpeed);
   }
   public getSpeed(): number {
-      return this.isNailed ? 0 : this.getBaseSpeedWithVoltage();
+      return this.isNailed ? 0 : this.baseSpeedWithVoltage;
   }
 }
