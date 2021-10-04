6f6a7c5c65016637ed3cd9789cdb6828b6c1f8e4
johan.martinsson <johan.martinsson@dexyos.fr>
2021-10-04 19:26:43 +0200

    assign new property

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -6,5 +6,6 @@ public class AfricanParrot extends Parrot {
 
     public AfricanParrot(ParrotTypeEnum african, int numberOfCoconuts, int i1, boolean b) {
         super(ParrotTypeEnum.AFRICAN, numberOfCoconuts, i1, b);
+        this.numberOfCoconuts = numberOfCoconuts;
     }
 }
