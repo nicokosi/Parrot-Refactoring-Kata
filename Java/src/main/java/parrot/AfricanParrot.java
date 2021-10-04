fc8dff54d0968b2e801c9be75d339d1b6fb438bb
johan.martinsson <johan.martinsson@dexyos.fr>
2021-10-04 19:21:05 +0200

    create class AfricanParrot

---
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -0,0 +1,7 @@
+package parrot;
+
+public class AfricanParrot extends Parrot {
+    public AfricanParrot(ParrotTypeEnum african, int i, int i1, boolean b) {
+        super(african, i, i1, b);
+    }
+}
