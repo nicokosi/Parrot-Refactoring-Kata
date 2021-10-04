910127f38a4d615506e93b7f0b45318fa1916ad3
johan.martinsson <johan.martinsson@dexyos.fr>
2021-10-04 19:27:13 +0200

    add override of getSpeed

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -8,4 +8,9 @@ public class AfricanParrot extends Parrot {
         super(ParrotTypeEnum.AFRICAN, numberOfCoconuts, i1, b);
         this.numberOfCoconuts = numberOfCoconuts;
     }
+
+    @Override
+    public double getSpeed() {
+        return super.getSpeed();
+    }
 }
