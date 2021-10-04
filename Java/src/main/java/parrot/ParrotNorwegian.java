5cdc8037cd7779e91f93524e6b1a2a6ce227313f
johan.martinsson <johan.martinsson@dexyos.fr>
2021-10-04 19:39:01 +0200

    override base class method, call superclass

--- Java/src/main/java/parrot/ParrotNorwegian.java
+++ Java/src/main/java/parrot/ParrotNorwegian.java
@@ -4,4 +4,9 @@ public class ParrotNorwegian extends Parrot {
     public ParrotNorwegian(ParrotTypeEnum norwegianBlue, double voltage, boolean isNailed) {
         super(norwegianBlue, voltage, isNailed);
     }
+
+    @Override
+    public double getSpeed() {
+        return super.getSpeed();
+    }
 }
