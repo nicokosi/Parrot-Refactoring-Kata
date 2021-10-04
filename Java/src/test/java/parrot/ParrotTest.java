8aa056c2d4849ada05800f30c3629b9f31a51a1c
johan.martinsson <johan.martinsson@dexyos.fr>
2021-10-04 19:45:36 +0200

    use norwegian in all tests

--- Java/src/test/java/parrot/ParrotTest.java
+++ Java/src/test/java/parrot/ParrotTest.java
@@ -38,13 +38,13 @@ public class ParrotTest {
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 1.5, false);
+        Parrot parrot = new ParrotNorwegian(ParrotTypeEnum.NORWEGIAN_BLUE, 1.5, false);
         assertEquals(18.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed_high_voltage() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 4, false);
+        Parrot parrot = new ParrotNorwegian(ParrotTypeEnum.NORWEGIAN_BLUE, 4, false);
         assertEquals(24.0, parrot.getSpeed(), 0.0);
     }
 }
