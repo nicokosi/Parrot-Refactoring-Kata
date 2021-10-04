8c97ed96f2cf7cf287a00005c5c9530164cd7fee
Jules H <jules.hablot@gmail.com>
2021-10-04 18:49:24 +0200

    introduce voltage params

--- TypeScript/test/parrot.spec.ts
+++ TypeScript/test/parrot.spec.ts
@@ -2,14 +2,14 @@ import { expect } from "chai";
 import "mocha";
 import { Parrot, ParrotTypes } from "../src/parrot";
 
-function createParrot(parrotType: ParrotTypes, numberOfCoconuts: number) {
-    return new Parrot(parrotType, numberOfCoconuts, 0, false);
+function createParrot(parrotType: ParrotTypes, numberOfCoconuts: number, voltage: number) {
+    return new Parrot(parrotType, numberOfCoconuts, voltage, false);
 }
 
 describe("Parrot", () => {
 
     it("gets speed of European Parrot", () => {
-        const parrot = createParrot(ParrotTypes.EUROPEAN, 0);
+        const parrot = createParrot(ParrotTypes.EUROPEAN, 0, 0);
 
         expect(parrot.getSpeed()).eql(12);
     });
