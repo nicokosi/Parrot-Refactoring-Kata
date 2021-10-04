7dfd16579636ea30adf09b2615a6bad7efcbb45b
Victor Lambret <lambret.victor@gmail.com>
2021-10-04 19:37:02 +0200

    Remove type EUROPEAN_PARROT

--- Python/parrot.py
+++ Python/parrot.py
@@ -2,7 +2,6 @@ from enum import Enum  # Enum is introduced in Python 3.4.
 
 
 class ParrotType(Enum):  # If it is not available, just remove it.
-    EUROPEAN = 1
     AFRICAN = 2
     NORWEGIAN_BLUE = 3
 
@@ -34,8 +33,6 @@ class Parrot:
         self._nailed = nailed
 
     def speed(self):
-        if self._type == ParrotType.EUROPEAN:
-            return EuropeanParrot().speed()
         if self._type == ParrotType.AFRICAN:
             return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
         if self._type == ParrotType.NORWEGIAN_BLUE:
