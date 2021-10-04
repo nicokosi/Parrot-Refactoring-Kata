58a0f5d98ffc0ba909e63a8c82a0f5e0b5e412fa
Jules H <jules.hablot@gmail.com>
2021-10-04 19:05:39 +0200

    create africanParrot subclass

--- TypeScript/src/parrot.ts
+++ TypeScript/src/parrot.ts
@@ -51,6 +51,16 @@ class EuropeanParrot extends Parrot {
     }
 }
 
+class AfricanParrot extends Parrot {
+    constructor(
+        numberOfCoconuts: number,
+        voltage: number,
+        isNailed: boolean,
+    ) {
+        super(ParrotTypes.AFRICAN, numberOfCoconuts, voltage, isNailed);
+    }
+}
+
 export function createParrot(
     parrotType: ParrotTypes,
     numberOfCoconuts: number,
