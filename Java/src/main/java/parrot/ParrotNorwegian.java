666ff1106da64ba303158d176196b357b2da6325
johan.martinsson <johan.martinsson@dexyos.fr>
2021-10-04 19:44:20 +0200

    implement norwegian

--- Java/src/main/java/parrot/ParrotNorwegian.java
+++ Java/src/main/java/parrot/ParrotNorwegian.java
@@ -12,6 +12,6 @@ public class ParrotNorwegian extends Parrot {
 
     @Override
     public double getSpeed() {
-        return super.getSpeed();
+        return (isNailed) ? 0 : getBaseSpeed(voltage);
     }
 }
