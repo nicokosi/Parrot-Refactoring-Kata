54ab6749ec6c2a3800f0ebe1e1cab31e04ba5d78
johan.martinsson <johan.martinsson@dexyos.fr>
2021-10-04 19:32:56 +0200

    remove type in african

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -4,7 +4,7 @@ public class AfricanParrot extends Parrot {
 
     private final int numberOfCoconuts;
 
-    public AfricanParrot(ParrotTypeEnum african, int numberOfCoconuts, int i1, boolean b) {
+    public AfricanParrot(int numberOfCoconuts, int i1, boolean b) {
         super(ParrotTypeEnum.AFRICAN, numberOfCoconuts, i1, b);
         this.numberOfCoconuts = numberOfCoconuts;
     }
