a66320de49c6b2306f18988b32f4099367ba4c49
johan.martinsson <johan.martinsson@dexyos.fr>
2021-10-04 19:17:06 +0200

    override but call the superclass

--- Java/src/main/java/parrot/EuropeanParrot.java
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -4,4 +4,9 @@ public class EuropeanParrot extends Parrot {
     public EuropeanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
         super(ParrotTypeEnum.EUROPEAN, numberOfCoconuts, voltage, isNailed);
     }
+
+    @Override
+    public double getSpeed() {
+        return super.getSpeed();
+    }
 }
