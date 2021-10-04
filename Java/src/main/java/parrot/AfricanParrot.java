c3e77ada8b9d1e555dc47a12f733bb5582c3b616
johan.martinsson <johan.martinsson@dexyos.fr>
2021-10-04 19:30:36 +0200

    make coconuts final

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -2,7 +2,7 @@ package parrot;
 
 public class AfricanParrot extends Parrot {
 
-    private int numberOfCoconuts;
+    private final int numberOfCoconuts;
 
     public AfricanParrot(ParrotTypeEnum african, int numberOfCoconuts, int i1, boolean b) {
         super(ParrotTypeEnum.AFRICAN, numberOfCoconuts, i1, b);
