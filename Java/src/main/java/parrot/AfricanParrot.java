bc5c88e4971ee6808aab87758fb33db0499af9f3
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2021-10-05 07:45:14 +0200

    Introduce AfricanParrot

---
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -0,0 +1,14 @@
+package parrot;
+
+public class AfricanParrot extends Parrot {
+
+    public AfricanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(type, numberOfCoconuts, voltage, isNailed);
+    }
+
+    @Override
+    public double getSpeed() {
+        return Math.max(0, getBaseSpeed() - getLoadFactor() * getNumberOfCoconuts());
+    }
+
+}
