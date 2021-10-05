aed0b7524adb5dcef2d4230d204a6ce0f34cbbfc
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2021-10-05 07:34:48 +0200

    Introduce EuropeanParrot

---
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -0,0 +1,13 @@
+package parrot;
+
+public class EuropeanParrot extends Parrot {
+
+    public EuropeanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(type, numberOfCoconuts, voltage, isNailed);
+    }
+
+    @Override
+    public double getSpeed() {
+        return super.getSpeed();
+    }
+}
