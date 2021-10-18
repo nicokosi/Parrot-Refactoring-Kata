b42ab212a81ce1661c980d2eac65458cdc17130b
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2021-10-18 07:28:06 +0200

    Inline field and format code

--- Kotlin/src/main/kotlin/parrot/Parrot.kt
+++ Kotlin/src/main/kotlin/parrot/Parrot.kt
@@ -14,14 +14,10 @@ data class EuropeanParrot(val baseSpeed: Double) : IParrot {
 data class AfricanParrot(val numberOfCoconuts: Int) : IParrot {
     private val baseSpeed = 12
     private val loadFactor = 9.0
-
     override fun speed(): Double = max(0.0, baseSpeed - loadFactor * numberOfCoconuts)
 }
 
 data class NorwegianBlueParrot(val voltage: Double, val isNailed: Boolean) : IParrot {
-    private val baseSpeed = 12
-
     override fun speed(): Double = if (isNailed) 0.0 else getBaseSpeed(voltage)
-
-    private fun getBaseSpeed(voltage: Double): Double = min(24.0, voltage * baseSpeed)
+    private fun getBaseSpeed(voltage: Double): Double = min(24.0, voltage * 12)
 }
