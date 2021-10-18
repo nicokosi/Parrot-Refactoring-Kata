dd43379e92ccca9dea5652de13b7bb771945f5ae
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2021-10-18 07:30:39 +0200

    Convert speed from function to property

--- Kotlin/src/main/kotlin/parrot/Parrot.kt
+++ Kotlin/src/main/kotlin/parrot/Parrot.kt
@@ -3,21 +3,24 @@ package parrot
 import kotlin.math.max
 import kotlin.math.min
 
-interface Parrot {
-    fun speed(): Double
+interface IParrot {
+    val speed: Double
 }
 
-data class EuropeanParrot(val baseSpeed: Double) : Parrot {
-    override fun speed(): Double = baseSpeed
+data class EuropeanParrot(val baseSpeed: Double) : IParrot {
+    override val speed: Double
+        get() = baseSpeed
 }
 
-data class AfricanParrot(val numberOfCoconuts: Int) : Parrot {
+data class AfricanParrot(val numberOfCoconuts: Int) : IParrot {
     private val baseSpeed = 12
     private val loadFactor = 9.0
-    override fun speed(): Double = max(0.0, baseSpeed - loadFactor * numberOfCoconuts)
+    override val speed: Double
+        get() = max(0.0, baseSpeed - loadFactor * numberOfCoconuts)
 }
 
-data class NorwegianBlueParrot(val voltage: Double, val isNailed: Boolean) : Parrot {
-    override fun speed(): Double = if (isNailed) 0.0 else getBaseSpeed(voltage)
+data class NorwegianBlueParrot(val voltage: Double, val isNailed: Boolean) : IParrot {
+    override val speed: Double
+        get() = if (isNailed) 0.0 else getBaseSpeed(voltage)
     private fun getBaseSpeed(voltage: Double): Double = min(24.0, voltage * 12)
 }
--- Kotlin/src/test/kotlin/parrot/ParrotTest.kt
+++ Kotlin/src/test/kotlin/parrot/ParrotTest.kt
@@ -9,42 +9,42 @@ class ParrotTest {
     @Test
     fun getSpeedOfEuropeanParrot() {
         val parrot = EuropeanParrot(12.0)
-        assertEquals(12.0, parrot.speed(), 0.0)
+        assertEquals(12.0, parrot.speed, 0.0)
     }
 
     @Test
     fun getSpeedOfAfricanParrot_With_One_Coconut() {
         val parrot = AfricanParrot( 1)
-        assertEquals(3.0, parrot.speed(), 0.0)
+        assertEquals(3.0, parrot.speed, 0.0)
     }
 
     @Test
     fun getSpeedOfAfricanParrot_With_Two_Coconuts() {
         val parrot = AfricanParrot(2)
-        assertEquals(0.0, parrot.speed(), 0.0)
+        assertEquals(0.0, parrot.speed, 0.0)
     }
 
     @Test
     fun getSpeedOfAfricanParrot_With_No_Coconuts() {
         val parrot = AfricanParrot(0)
-        assertEquals(12.0, parrot.speed(), 0.0)
+        assertEquals(12.0, parrot.speed, 0.0)
     }
 
     @Test
     fun getSpeedNorwegianBlueParrot_nailed() {
         val parrot = NorwegianBlueParrot(1.5, true)
-        assertEquals(0.0, parrot.speed(), 0.0)
+        assertEquals(0.0, parrot.speed, 0.0)
     }
 
     @Test
     fun getSpeedNorwegianBlueParrot_not_nailed() {
         val parrot = NorwegianBlueParrot(1.5, false)
-        assertEquals(18.0, parrot.speed(), 0.0)
+        assertEquals(18.0, parrot.speed, 0.0)
     }
 
     @Test
     fun getSpeedNorwegianBlueParrot_not_nailed_high_voltage() {
         val parrot = NorwegianBlueParrot(4.0, false)
-        assertEquals(24.0, parrot.speed(), 0.0)
+        assertEquals(24.0, parrot.speed, 0.0)
     }
 }
