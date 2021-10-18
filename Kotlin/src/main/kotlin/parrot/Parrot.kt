6463d7eb8998db5af55d3a271f44968ab4548e48
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2021-10-18 07:31:54 +0200

    No I* interfaces!

--- Kotlin/src/main/kotlin/parrot/Parrot.kt
+++ Kotlin/src/main/kotlin/parrot/Parrot.kt
@@ -3,23 +3,23 @@ package parrot
 import kotlin.math.max
 import kotlin.math.min
 
-interface IParrot {
+interface Parrot {
     val speed: Double
 }
 
-data class EuropeanParrot(val baseSpeed: Double) : IParrot {
+data class EuropeanParrot(val baseSpeed: Double) : Parrot {
     override val speed: Double
         get() = baseSpeed
 }
 
-data class AfricanParrot(val numberOfCoconuts: Int) : IParrot {
+data class AfricanParrot(val numberOfCoconuts: Int) : Parrot {
     private val baseSpeed = 12
     private val loadFactor = 9.0
     override val speed: Double
         get() = max(0.0, baseSpeed - loadFactor * numberOfCoconuts)
 }
 
-data class NorwegianBlueParrot(val voltage: Double, val isNailed: Boolean) : IParrot {
+data class NorwegianBlueParrot(val voltage: Double, val isNailed: Boolean) : Parrot {
     override val speed: Double
         get() = if (isNailed) 0.0 else getBaseSpeed(voltage)
     private fun getBaseSpeed(voltage: Double): Double = min(24.0, voltage * 12)
