be94a2357943c63a0981db5a2e0367d0329f6e7d
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2021-10-18 07:24:41 +0200

    Delete dead code

--- Kotlin/src/main/kotlin/parrot/Parrot.kt
+++ Kotlin/src/main/kotlin/parrot/Parrot.kt
@@ -24,28 +24,4 @@ data class NorwegianBlueParrot(val voltage: Double, val isNailed: Boolean) : IPa
     override fun speed(): Double = if (isNailed) 0.0 else getBaseSpeed(voltage)
 
     private fun getBaseSpeed(voltage: Double): Double = min(24.0, voltage * baseSpeed)
-
-}
-
-open class Parrot(
-    private val type: ParrotTypeEnum,
-    private val numberOfCoconuts: Int,
-    private val voltage: Double,
-    private val isNailed: Boolean
-) {
-
-    val speed: Double
-        get() = when (type) {
-            ParrotTypeEnum.EUROPEAN -> baseSpeed
-            ParrotTypeEnum.AFRICAN -> max(0.0, baseSpeed - loadFactor * numberOfCoconuts)
-            ParrotTypeEnum.NORWEGIAN_BLUE -> if (isNailed) 0.0 else getBaseSpeed(voltage)
-        }
-
-    private val loadFactor: Double
-        get() = 9.0
-
-    private val baseSpeed: Double
-        get() = 12.0
-
-    private fun getBaseSpeed(voltage: Double): Double = min(24.0, voltage * baseSpeed)
 }
